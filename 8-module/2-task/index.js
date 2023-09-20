631abc9b7f60c14af64da0d80a95c7f823adc561
Oleksandr Dikhtiar <dikhtyar.a@gmail.com>
2023-09-20 10:40:36 +0400

    need to solve check test problems in 8-1 and 8-3

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -6,29 +6,6 @@ export default class ProductGrid {
     this.products = products;
     this.filters = {};
     this.render();
-    this.sortContent();
-    this.checkChange();
-  }
-
-  sortContent() {
-    this.elem.querySelector('.products-grid__inner').innerHTML = '';
-    this.products.forEach(element => {
-      if (this.filters.noNuts && element.nuts) {return;}
-      if (this.filters.vegeterianOnly && !element.vegeterian) {return;}
-      if (undefined !== this.filters.maxSpiciness && element.spiciness > this.filters.maxSpiciness) {return;}
-      if (this.filters.category && element.category != this.filters.category) {return;}
-      let card = new ProductCard(element);
-      this.elem.querySelector('.products-grid__inner').appendChild(card.elem);
-    });
-  }
-
-  updateFilter(filters) {
-    Object.assign(this.filters, filters);
-    document.querySelector('.controls').addEventListener('change', this.sortContent());
-  }
-  
-  checkChange() {
-    document.querySelector('.controls input').addEventListener('change', this.updateFilter());
   }
 
   render() {
--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -3,27 +3,73 @@ export default class Cart {
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
-  }
 
-  addProduct(product) {
-    // ваш код
-  }
-
-  updateProductCount(productId, amount) {
-    // ваш код
   }
 
   isEmpty() {
     // ваш код
+    if (this.cartItems.length === 0) {
+      //console.log('пустой');
+      return true;
+    }
   }
+
+  addProduct(product) {
+    if (product !== null && product) {
+      let q = this.cartItems.find(e => e.product.id === product.id);
+      if (q !== undefined) { q.count += 1;}
+      else {this.cartItems.push({product: product, count: 1});}
+      //console.log(this.cartItems, this.cartItems.length);
+      //this.updateProductCount();
+      //this.onProductUpdate(cartItem);
+    }
+  }
+
+  updateProductCount(productId, amount) {
+    let q = this.cartItems.find(e => productId === e.product.id);
+    let ind = this.cartItems.indexOf(q);
+    let newCount = q.count + amount;
+    q.count = newCount;
+    if (q.count == 0) 
+    { 
+      //console.log('удалён');
+      this.cartItems.splice(ind, 1);
+    } 
+    else {
+      //console.log('изменение количества', q.count);
+      return q.count;
+    }
+    //this.getTotalCount();
+    //this.getTotalPrice();
+    //this.onProductUpdate();
+  }
+  
+
 
   getTotalCount() {
     // ваш код
+    if (this.isEmpty()) { 
+      return 0;
+    } 
+    else {
+      let sum = 0;
+      this.cartItems.forEach(e => sum += e.count);
+      return sum;
+    }
   }
 
   getTotalPrice() {
-    // ваш код
+    if (this.isEmpty()) { 
+      return 0;
+    } 
+    else {
+      let cost = 0;
+      this.cartItems.forEach(e => cost = cost + (e.count * e.product.price));
+      //console.log(cost);
+      return cost;
+    }
   }
+  
 
   onProductUpdate(cartItem) {
     // реализуем в следующей задаче
